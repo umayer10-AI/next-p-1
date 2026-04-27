0cc298c8bc6b61f178f9f0344efdd53df75d6d7b
umayer10-AI <mdumayer10@gmail.com>
2026-04-27 15:40:53 +0600

    add

---
+++ src/lib/auth.js
@@ -0,0 +1,15 @@
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+const client = new MongoClient("mongodb://localhost:27017/database");
+const db = client.db("my-new-drag");
+
+export const auth = betterAuth({
+    database: mongodbAdapter(db, {
+        client
+    }),
+    emailAndPassword: { 
+        enabled: true, 
+    },
+});
