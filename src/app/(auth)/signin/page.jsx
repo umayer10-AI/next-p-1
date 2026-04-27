1ef8b7584d8a6349db2c7b6d1bc6a778efde6b2f
umayer10-AI <mdumayer10@gmail.com>
2026-04-27 15:34:56 +0600

    add

--- src/app/(auth)/signin/page.jsx
+++ src/app/(auth)/signin/page.jsx
@@ -1,19 +1,36 @@
+"use client"
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 const LoginPage = () => {
+
+    const {register,handleSubmit,watch,formState: { errors }} = useForm()
+
+    const [c,setC] = useState(false)
+
+    const a = (v) => {
+        console.log(v)
+    }
+
+    console.log(errors)
+
     return (
          <div>
             <div className='flex justify-center mt-5 mb-10'>
-                <form className="fieldset bg-base-200 border-base-300 rounded-box w-md border p-8">
+                <form onSubmit={handleSubmit(a)} className="fieldset bg-base-200 border-base-300 rounded-box w-md border p-8">
 
-                    <h2 className='text-3xl font-semibold text-center mb-5'>Register your account</h2>
+                    <h2 className='text-3xl font-semibold text-center mb-5'>Login your account</h2>
 
                     <label className="label text-black font-semibold text-base">Email address</label>
-                    <input type="email" className="input w-full" placeholder="Enter email address" />
+                    <input type="email" className="input w-full" placeholder="Enter email address" {...register("email", { required: "Email filed required" })} />
+                    {errors.email && <p className='text-red-600 font-semibold'>{errors.email.message}</p>}
 
                     <label className="label text-black font-semibold text-base">Password</label>
-                    <input type="password" className="input w-full" placeholder="Enter your password" />
+                    <input type={c?"text":"password"} className="input w-full" placeholder="Enter your password"  {...register("password", { required: "Password filed required" })} />
+                    <span onClick={() => setC(!c)}>{c ? <FaEye></FaEye> : <FaEyeSlash></FaEyeSlash>}</span>
+                    {errors.password && <p className='text-red-600 font-semibold'>{errors.password.message}</p>}
 
                     <button className="btn btn-neutral my-4">Login</button>
 
--- src/app/(auth)/signup/page.jsx
+++ src/app/(auth)/signup/page.jsx
@@ -1,24 +1,45 @@
-import Navbar from '@/component/Navbar';
-import React from 'react';
+"use client"
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 const SignUpPage = () => {
+
+    const {register,handleSubmit,watch,formState: { errors }} = useForm()
+
+    const [c,setC] = useState(false)
+
+    const a = (v) => {
+        console.log(v)
+    }
+
+    console.log(errors)
+
     return (
          <div>
             <div className='flex justify-center mt-5 mb-10'>
-                <form className="fieldset bg-base-200 border-base-300 rounded-box w-md border p-8">
+                <form onSubmit={handleSubmit(a)} className="fieldset bg-base-200 border-base-300 rounded-box w-md border p-8">
 
                     <h2 className='text-3xl font-semibold text-center mb-5'>Register your account</h2>
 
+                    <label className="label text-black font-semibold text-base">Name</label>
+                    <input type="name" className="input w-full" placeholder="Enter name address" {...register("name", { required: "Name filed required" })} />
+                    {errors.name && <p className='text-red-600 font-semibold'>{errors.name.message}</p>}
+
+                    <label className="label text-black font-semibold text-base">Photo url</label>
+                    <input type="url" className="input w-full" placeholder="Enter url address" {...register("url", { required: "URL filed required" })} />
+                    {errors.url && <p className='text-red-600 font-semibold'>{errors.url.message}</p>}
+
                     <label className="label text-black font-semibold text-base">Email address</label>
-                    <input type="email" className="input w-full" placeholder="Enter email address" />
+                    <input type="email" className="input w-full" placeholder="Enter email address" {...register("email", { required: "Email filed required" })} />
+                    {errors.email && <p className='text-red-600 font-semibold'>{errors.email.message}</p>}
 
                     <label className="label text-black font-semibold text-base">Password</label>
-                    <input type="password" className="input w-full" placeholder="Enter your password" />
+                    <input type={c?"text":"password"} className="input w-full" placeholder="Enter your password"  {...register("password", { required: "Password filed required" })} />
+                    <span onClick={() => setC(!c)}>{c ? <FaEye></FaEye> : <FaEyeSlash></FaEyeSlash>}</span>
+                    {errors.password && <p className='text-red-600 font-semibold'>{errors.password.message}</p>}
 
-                    <button className="btn btn-neutral my-4">Login</button>
-
-                    <p className='text-center font-semibold text-base'>Dont’t Have An Account ? <span className='
-                    text-red-600'>Register</span></p>
+                    <button className="btn btn-neutral my-4">Register</button>
                 </form>
             </div>
         </div>
