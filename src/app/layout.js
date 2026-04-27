2a87ae9867cb117f7a6a5db6374443f65a9a44ff
umayer10-AI <mdumayer10@gmail.com>
2026-04-27 12:13:26 +0600

    add

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/component/Navbar";
+import Header from "@/component/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,6 +25,7 @@ export default function RootLayout({ children }) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        <Header></Header>
         <Navbar></Navbar>
         {children}
         </body>
--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import img from "../assets/logo.png"
+import Image from 'next/image';
 
 const Header = () => {
     return (
-        <div>
-            
+        <div className='my-5'>
+            <div>
+                <Image height={320} width={320} src={img} alt='logo' className='mx-auto'></Image>
+            </div>
+            <h2 className='text-gray-500 font-semibold'>Journalism Without Fear or Favour</h2>
+            <h2>{format(new Date(2014, 1, 11), "yyyy-MM-dd")}</h2>
         </div>
     );
 };
