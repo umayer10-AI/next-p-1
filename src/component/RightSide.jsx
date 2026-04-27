4dac8862fc804f3e15eddf7022d8f041a82c986d
umayer10-AI <mdumayer10@gmail.com>
2026-04-27 18:03:59 +0600

    add

--- src/component/RightSide.jsx
+++ src/component/RightSide.jsx
@@ -2,6 +2,10 @@
 import { authClient } from '@/lib/auth-client';
 import React from 'react';
 import { BsFacebook, BsInstagram, BsTwitter } from 'react-icons/bs';
+import img1 from "../assets/swimming.png"
+import img2 from "../assets/class.png"
+import img3 from "../assets/playground.png"
+import Image from 'next/image';
 
 const RightSide = () => {
 
@@ -38,14 +42,23 @@ const RightSide = () => {
                     Login with Facebook
                 </button>
             </div>
+
             <div className="my-10">
-            <h2 className="text-xl font-bold mb-3">Find Us On</h2>
-            <div>
-                <h2 className="flex items-center gap-2 text-xl font-semibold text-gray-600 btn"><BsFacebook className="text-blue-600"/>Facebook</h2>
-                <h2 className="flex items-center gap-2 text-xl font-semibold text-gray-600 btn"><BsTwitter className="text-blue-400"/>Twitter</h2>
-                <h2 className="flex items-center gap-2 text-xl font-semibold text-gray-600 btn"><BsInstagram className="text-pink-600"/>Instagram</h2>
+                <h2 className="text-xl font-bold mb-3">Find Us On</h2>
+                <div>
+                    <h2 className="flex items-center gap-2 text-xl font-semibold text-gray-600 btn"><BsFacebook className="text-blue-600"/>Facebook</h2>
+                    <h2 className="flex items-center gap-2 text-xl font-semibold text-gray-600 btn"><BsTwitter className="text-blue-400"/>Twitter</h2>
+                    <h2 className="flex items-center gap-2 text-xl font-semibold text-gray-600 btn"><BsInstagram className="text-pink-600"/>Instagram</h2>
                 </div>
             </div>
+
+            <div className="my-10 bg-gray-100 p-3">
+                <h2 className="text-xl font-bold mb-3">Q-Zone</h2>
+                <Image src={img1} alt="logo"></Image>
+                <Image src={img2} alt="logo"></Image>
+                <Image src={img3} alt="logo"></Image>
+            </div>
+
         </div>
     );
 };
