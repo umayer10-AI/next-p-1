6e1c52aae432b0b4b58c7036f58e91af27d81ac7
umayer10-AI <mdumayer10@gmail.com>
2026-04-27 15:58:27 +0600

    add

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,8 +1,16 @@
+"use client"
 import React from 'react';
 import NavLink from './NavLink';
 import { HiUserCircle } from 'react-icons/hi';
+import { authClient } from '@/lib/auth-client';
+import Link from 'next/link';
 
 const Navbar = () => {
+
+    const { data: session } = authClient.useSession()
+
+    const user = session?.user
+
     return (
         <div className='grid grid-cols-1 lg:grid-cols-3 w-[80%] mx-auto my-5'>
             <div className='hidden md:flex'>
@@ -14,8 +22,14 @@ const Navbar = () => {
             </div>
 
             <div className='flex justify-end items-center gap-2'>
+                <h2 className='font-semibold'>{user && user.name}</h2>
                 <h2 className='text-5xl'><HiUserCircle /></h2>
-                <button className='btn bg-neutral text-white px-7'>Login</button>
+
+                {
+                    user? 
+                    <button onClick={async () =>await authClient.signOut()} className='btn btn-primary'>LogOut</button>
+                    : <Link href={'/signin'} className='btn bg-neutral text-white px-7'>Login</Link>
+                }
             </div>
         </div>
     );
